9fa499e4a1400355e5866b05eb43d4a4b2197010
pio <piyushjadhav13@gmail.com>
2021-06-18 06:39:41 +0100

    add cpp file.

---
+++ q1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> esort( vector<int> data )
+{   
+    // sort
+
+    return data;
+}
+
+
+int main ( void )
+{
+    vector<int> test_vector;
+
+    // we  will build a test vector to pass to our soting mechanism
+    // build
+
+
+    test_vector = esort( test_vector );
+
+
+    return 0;
+} 
