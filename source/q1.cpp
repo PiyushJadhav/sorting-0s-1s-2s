81a380c70caef3c085b1a0c7cef7660afd81b3fb
PiyJ <Piyush.Jadhav@dyson.com>
2021-06-18 08:47:15 +0100

    add test within main.

--- source/q1.cpp
+++ source/q1.cpp
@@ -1,12 +1,14 @@
 /**
  * @brief This file sorts an vector of elements of 0, 1 and 2.
- * The main() build a dummy vector, which is given to the esort() sorting functions. 
- * 
+ * The main() build a dummy vector, which is given to the esort() sorting functions.
+ *
  */
 
 // include
 #include <iostream>
 #include <vector>
+
+#include <algorithm> // only used for testing
 
 // namespace
 using namespace std;
@@ -16,20 +18,20 @@ static vector<int> esort( vector<int> data );
 
 /**
  * This function sorts the input vector conatining only 0s, 1s and 2s.
- * Our logic would be to start filling 0s from the start of the vector and 2s from the end vector. 
+ * Our logic would be to start filling 0s from the start of the vector and 2s from the end vector.
  * We will use standard swap() to swap values within the vector.
  * Note it throws exceptions when the elements are not either 0, 1 or 2s and exit app.
- * param[in] data data vector which is to be sorted. 
+ * param[in] data data vector which is to be sorted.
  * return data sorted data vector
  */
 static vector<int> esort( vector<int> data )
-{   
+{
     const int N = data.size();
 
     // iterators
     int i0 = 0;
     int i2 = N-1;
-    
+
     // itr for iterating over the vector
     int i = 0;
     try
@@ -54,7 +56,7 @@ static vector<int> esort( vector<int> data )
                     i++;
                 }
                 break;
-                
+
                 case 2:
                 {
                     swap(data.at(i), data.at(i2));
@@ -89,22 +91,31 @@ int main ( void )
     // we  will build a test vector to pass to our sorting mechanism
     // build
     int N = 30;
-    
+
     for( int i=0; i<N; i++ )
     {
         int a = i%3;
         test_vector.push_back(a);
     }
 
+    // store vec to test with
+    vector<int> test_with(test_vector);
+
     test_vector = esort( test_vector );
 
-    // print out the vector to check 
-    for( int i = 0; i < N; i++ )
-    {
-        cout << test_vector.at(i) << ", ";
-    }
+    // test
+    // sort the test_with
+    sort(test_with.begin(), test_with.end() );
+    bool result = equal(test_vector.begin(), test_vector.end(), test_with.begin() );
 
-    cout << endl;
+    if( result )
+    {
+        cout << "Test successful" << endl;
+    }
+    else
+    {
+        cout << "Test fail." << endl;
+    }
 
     return 0;
 }
